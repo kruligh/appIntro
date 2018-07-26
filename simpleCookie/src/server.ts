5ba64d1665b50210f9a4f16e0b38a73af68ac870
Dominik Kroliczek <dominik@kroliczek.com>
2018-07-26 22:53:35 +0200

    Add simple cookie example

---
+++ simpleCookie/src/server.ts
@@ -0,0 +1,30 @@
+import * as express from "express";
+import {Express, NextFunction, Request, Response} from "express";
+import * as bodyParser from "body-parser";
+import * as http from "http";
+import * as serveStatic from 'serve-static';
+
+const PORT = 3000;
+const STATIC_DIR = `./public`;
+const app: Express = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
+    next();
+});
+
+app.use(serveStatic(STATIC_DIR, {'index': ['index.html']}));
+
+app.listen(PORT, () => {
+    console.log("Server started on port " + PORT + " server static from " + STATIC_DIR);
+});
+
+/*app.get('/room/:roomId', (req: Request, res: Response, next: Function) => {
+    res.status(200).json({
+        status: 'success',
+        data: clientService.getRoom(req.params.roomId),
+    });
+    next();
+});*/
